76381e2c2a5d80cf036c1f78bb84dbde651188f1
Paul Pruenster <paul.pruenster@gmail.com>
2022-04-04 20:37:10 +0200

    erster toel fe dor 2.

---
+++ e4/task2/ls_wc.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(void)
+{
+    int fd[2];
+
+    if (pipe(fd) < 0) {
+        printf("Pipe error");
+        exit(1);
+    }
+    pid_t pid1 = fork();
+    if (pid1 < 0) {
+        printf("Fork error");
+        exit(1);
+    }
+    if (pid1 == 0) // Child1
+    {
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+
+        // Uses the PATH variable, doesnt need the absolute path
+        execlp("ls", "ls", NULL, NULL);
+        exit(1);
+    }
+
+    // Parent
+    pid_t pid2 = fork();
+    if (pid2 < 0)
+    {
+        printf("Fork error");
+        exit(1);
+    }
+    if (pid2 == 0) // Child2
+    {
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+
+        execlp("wc", "wc", "-l", NULL);
+        exit(1);
+    }
+
+    // Parent
+    close(fd[0]);
+    close(fd[1]);
+
+    wait(NULL);
+    return 0;
+}
