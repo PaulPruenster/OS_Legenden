8885dd2b3f459bddd2a5b051ea8714cb685deb5e
Paul Pruenster <paul.pruenster@gmail.com>
2022-04-26 11:58:16 +0200

    laft amol clean

--- e5/task2/task2.c
+++ e5/task2/task2.c
@@ -8,8 +8,11 @@
 #include <sys/wait.h>
 #include <semaphore.h>
 
-#define WRITE 0
-#define CONSUME 1
+#define WRITE shared_mem[0]
+#define CONSUME shared_mem[1]
+
+#define CONTENT 0
+#define SEM 1
 
 typedef struct data
 {
@@ -22,22 +25,34 @@ typedef struct sema
 {
   int fd;
   sem_t *shared_mem;
+  uint64_t shared_mem_size;
 } SemaData;
+
+void close_shm(ThreadData *structdata, SemaData *semadata)
+{
+  sem_destroy(&semadata->WRITE);
+  sem_destroy(&semadata->CONSUME);
+
+  munmap(structdata->shared_mem, structdata->shared_mem_size);
+  munmap(semadata->shared_mem, semadata->shared_mem_size);
+
+  close(structdata->fd);
+  close(semadata->fd);
+}
 
 void writer(uint64_t n, uint64_t b, ThreadData *structdata, SemaData *semadata)
 {
   for (uint64_t i = 0; i < n; ++i)
   {
-    sem_wait(&semadata->shared_mem[WRITE]);
+    sem_wait(&semadata->WRITE);
     if (n >= b)
       structdata->shared_mem[i % b] = i + 1;
     else
       structdata->shared_mem[i] = i + 1;
-    sem_post(&semadata->shared_mem[CONSUME]);
+    sem_post(&semadata->CONSUME);
   }
 
-  munmap(structdata->shared_mem, structdata->shared_mem_size);
-  close(structdata->fd);
+  close_shm(structdata, semadata);
 }
 
 uint64_t reader(uint64_t n, uint64_t b, ThreadData *structdata, SemaData *semadata)
@@ -45,19 +60,18 @@ uint64_t reader(uint64_t n, uint64_t b, ThreadData *structdata, SemaData *semada
   uint64_t sum = 0;
   for (uint64_t i = 0; i < n; ++i)
   {
-    sem_wait(&semadata->shared_mem[CONSUME]);
+    sem_wait(&semadata->CONSUME);
     if (n >= b)
       sum += structdata->shared_mem[i % b];
     else
       sum += structdata->shared_mem[i];
-    sem_post(&semadata->shared_mem[WRITE]);
+    sem_post(&semadata->WRITE);
   }
-  munmap(structdata->shared_mem, structdata->shared_mem_size);
-  close(structdata->fd);
+  close_shm(structdata, semadata);
   return sum;
 }
 
-void *initialize_shared_mem(const char *name, const uint64_t shared_mem_size)
+void *initialize_shared_mem(const char *name, const uint64_t shared_mem_size, const int mode)
 {
   const int oflag = O_CREAT | O_EXCL | O_RDWR; // fail if name already exists, read+write
   const mode_t permission = S_IRUSR | S_IWUSR; // 600
@@ -75,17 +89,29 @@ void *initialize_shared_mem(const char *name, const uint64_t shared_mem_size)
     return NULL;
   }
 
-  uint64_t *shared_mem = mmap(NULL, shared_mem_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  void *shared_mem = mmap(NULL, shared_mem_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
   if (shared_mem == MAP_FAILED)
   {
     perror("mmap");
     return NULL;
   }
-  ThreadData *structdata = malloc(sizeof(structdata));
-  structdata->fd = fd;
-  structdata->shared_mem = shared_mem;
-  structdata->shared_mem_size = shared_mem_size;
-  return structdata;
+  if (mode == CONTENT)
+  {
+    ThreadData *structdata = malloc(sizeof(structdata));
+    structdata->fd = fd;
+    structdata->shared_mem = shared_mem;
+    structdata->shared_mem_size = shared_mem_size;
+    return structdata;
+  }
+  else
+  {
+    SemaData *structdata = malloc(sizeof(structdata));
+    structdata->fd = fd;
+    structdata->shared_mem = shared_mem;
+    structdata->shared_mem_size = shared_mem_size;
+    return structdata;
+  }
 }
 
 int main(int argc, char *argv[])
@@ -104,17 +130,17 @@ int main(int argc, char *argv[])
 
   char *name_sem = "/shared_mem_sem";
   const int shared_mem_size_sem = sizeof(sem_t) * 2;
-  SemaData *semadata = initialize_shared_mem(name_sem, shared_mem_size_sem);
+  SemaData *semadata = initialize_shared_mem(name_sem, shared_mem_size_sem, SEM);
   if (semadata == NULL)
   {
     return EXIT_FAILURE;
   }
-  sem_init(&semadata->shared_mem[WRITE], 1, 1);
-  sem_init(&semadata->shared_mem[CONSUME], 1, 0);
+  sem_init(&semadata->WRITE, 1, 1);
+  sem_init(&semadata->CONSUME, 1, 0);
 
   char *name = "/shared_mem";
   const uint64_t shared_mem_size = b * sizeof(uint64_t);
-  ThreadData *structdata = initialize_shared_mem(name, shared_mem_size);
+  ThreadData *structdata = initialize_shared_mem(name, shared_mem_size, CONTENT);
   if (structdata == NULL)
   {
     return EXIT_FAILURE;
@@ -140,29 +166,17 @@ int main(int argc, char *argv[])
 
   if (reader_proc == 0)
   {
-    printf("%llu", reader(n, b, structdata, semadata));
+    printf("%lu\n", reader(n, b, structdata, semadata));
     exit(0);
   }
 
   wait(NULL); // wait for both forks
-
-  sem_destroy(&semadata->shared_mem[WRITE]);
-  sem_destroy(&semadata->shared_mem[CONSUME]);
-  munmap(structdata->shared_mem, shared_mem_size);
-  close(structdata->fd);
+  close_shm(structdata, semadata);
   shm_unlink(name);     // delete shared memory
   shm_unlink(name_sem); // delete shared memory
 
   return EXIT_SUCCESS;
 }
 /*	Observations:
- *  -> If you quit your program clean (close and unlink the file/shared memory segment)
- *     you can get the correct answer if you are a lucky and n < b. But there is a problem, if N > B you have
- *     to use the ring buffer. Due to the implementation, the ring buffer doesn't work
- *     as expected, because it doesn't wait for the reader to read the value and then
- *     overrides it. It overrides it, nevertheless the reader has read it or not.
- *
- *  -> If you try really large numbers for N and B, it is likely to run into an overflow or even the
- *     program will crash, due to a too big number and end in an SIGSEV.
  *
  */
