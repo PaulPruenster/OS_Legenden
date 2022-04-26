c99239b4f24bb436daa24f70b65c88ab154016ab
Paul Pruenster <paul.pruenster@gmail.com>
2022-04-26 11:37:42 +0200

    2. laft obor net olles wert gefeed

--- e5/task2/task2.c
+++ e5/task2/task2.c
@@ -8,76 +8,56 @@
 #include <sys/wait.h>
 #include <semaphore.h>
 
-void writer(uint64_t n, uint64_t b, uint64_t shared_mem_size, const char *name, sem_t write, sem_t consume)
+#define WRITE 0
+#define CONSUME 1
+
+typedef struct data
 {
+  int fd;
+  uint64_t *shared_mem;
+  uint64_t shared_mem_size;
+} ThreadData;
 
-  const int fd = shm_open(name, O_RDWR, S_IRUSR | S_IWUSR);
+typedef struct sema
+{
+  int fd;
+  sem_t *shared_mem;
+} SemaData;
 
-  if (fd < 0)
-  {
-    perror("shm_open");
-    return;
-  }
-
-  uint64_t *shared_mem = mmap(NULL, shared_mem_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-
-  if (shared_mem == MAP_FAILED)
-  {
-    perror("mmap");
-    return;
-  }
-
+void writer(uint64_t n, uint64_t b, ThreadData *structdata, SemaData *semadata)
+{
   for (uint64_t i = 0; i < n; ++i)
   {
-    sem_wait(&write);
+    sem_wait(&semadata->shared_mem[WRITE]);
     if (n >= b)
-      shared_mem[i % b] = i + 1;
+      structdata->shared_mem[i % b] = i + 1;
     else
-      shared_mem[i] = i + 1;
-    sem_post(&consume);
+      structdata->shared_mem[i] = i + 1;
+    sem_post(&semadata->shared_mem[CONSUME]);
   }
 
-  sem_destroy(&write);
-  sem_destroy(&consume);
-  munmap(shared_mem, shared_mem_size);
-  close(fd);
+  munmap(structdata->shared_mem, structdata->shared_mem_size);
+  close(structdata->fd);
 }
 
-uint64_t reader(uint64_t n, uint64_t b, uint64_t shared_mem_size, const char *name, sem_t write, sem_t consume)
+uint64_t reader(uint64_t n, uint64_t b, ThreadData *structdata, SemaData *semadata)
 {
-  const int fd = shm_open(name, O_RDWR, 0);
-  if (fd < 0)
-  {
-    perror("shm_open");
-    return EXIT_FAILURE;
-  }
-
-  uint64_t *shared_mem = mmap(NULL, shared_mem_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-  if (shared_mem == MAP_FAILED)
-  {
-    perror("mmap");
-    return EXIT_FAILURE;
-  }
-
   uint64_t sum = 0;
   for (uint64_t i = 0; i < n; ++i)
   {
-    sem_wait(&consume);
+    sem_wait(&semadata->shared_mem[CONSUME]);
     if (n >= b)
-      sum += shared_mem[i % b];
+      sum += structdata->shared_mem[i % b];
     else
-      sum += shared_mem[i];
-    sem_post(&write);
+      sum += structdata->shared_mem[i];
+    sem_post(&semadata->shared_mem[WRITE]);
   }
-
-  sem_destroy(&write);
-  sem_destroy(&consume);
-  munmap(shared_mem, shared_mem_size);
-  close(fd);
+  munmap(structdata->shared_mem, structdata->shared_mem_size);
+  close(structdata->fd);
   return sum;
 }
 
-int initialize_shared_mem(const char *name, const uint64_t shared_mem_size)
+void *initialize_shared_mem(const char *name, const uint64_t shared_mem_size)
 {
   const int oflag = O_CREAT | O_EXCL | O_RDWR; // fail if name already exists, read+write
   const mode_t permission = S_IRUSR | S_IWUSR; // 600
@@ -86,25 +66,26 @@ int initialize_shared_mem(const char *name, const uint64_t shared_mem_size)
   if (fd < 0)
   {
     perror("shm_open");
-    return EXIT_FAILURE;
+    return NULL;
   }
 
   if (ftruncate(fd, shared_mem_size) != 0)
   {
     perror("ftruncate");
-    return EXIT_FAILURE;
+    return NULL;
   }
 
   uint64_t *shared_mem = mmap(NULL, shared_mem_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
   if (shared_mem == MAP_FAILED)
   {
     perror("mmap");
-    return EXIT_FAILURE;
+    return NULL;
   }
-
-  munmap(shared_mem, shared_mem_size);
-  close(fd);
-  return EXIT_SUCCESS;
+  ThreadData *structdata = malloc(sizeof(structdata));
+  structdata->fd = fd;
+  structdata->shared_mem = shared_mem;
+  structdata->shared_mem_size = shared_mem_size;
+  return structdata;
 }
 
 int main(int argc, char *argv[])
@@ -115,21 +96,29 @@ int main(int argc, char *argv[])
     return EXIT_FAILURE;
   }
 
-  uint64_t n = strtol(argv[1], NULL, 10);
-  uint64_t b = strtol(argv[2], NULL, 10);
+  char *end1 = NULL;
+  char *end2 = NULL;
 
-  char *name = "/shared_mem";
-  const uint64_t shared_mem_size = b * sizeof(uint64_t) + 2 * sizeof(sem_t);
+  uint64_t n = strtol(argv[1], &end1, 10);
+  uint64_t b = strtol(argv[2], &end2, 10);
 
-  if (initialize_shared_mem(name, shared_mem_size) != EXIT_SUCCESS)
+  char *name_sem = "/shared_mem_sem";
+  const int shared_mem_size_sem = sizeof(sem_t) * 2;
+  SemaData *semadata = initialize_shared_mem(name_sem, shared_mem_size_sem);
+  if (semadata == NULL)
   {
     return EXIT_FAILURE;
   }
+  sem_init(&semadata->shared_mem[WRITE], 1, 1);
+  sem_init(&semadata->shared_mem[CONSUME], 1, 0);
 
-  sem_t write;
-  sem_t consume;
-  sem_init(&write, 1, 1);
-  sem_init(&consume, 1, 1);
+  char *name = "/shared_mem";
+  const uint64_t shared_mem_size = b * sizeof(uint64_t);
+  ThreadData *structdata = initialize_shared_mem(name, shared_mem_size);
+  if (structdata == NULL)
+  {
+    return EXIT_FAILURE;
+  }
 
   const pid_t writer_proc = fork();
   if (writer_proc == -1)
@@ -139,7 +128,7 @@ int main(int argc, char *argv[])
 
   if (writer_proc == 0)
   {
-    writer(n, b, shared_mem_size, name, write, consume);
+    writer(n, b, structdata, semadata);
     exit(0);
   }
 
@@ -151,16 +140,29 @@ int main(int argc, char *argv[])
 
   if (reader_proc == 0)
   {
-    printf("%llu", reader(n, b, shared_mem_size, name, write, consume));
+    printf("%llu", reader(n, b, structdata, semadata));
     exit(0);
   }
 
   wait(NULL); // wait for both forks
-  wait(NULL);
 
-  sem_destroy(&write);
-  sem_destroy(&consume);
-  shm_unlink(name); // delete shared memory
+  sem_destroy(&semadata->shared_mem[WRITE]);
+  sem_destroy(&semadata->shared_mem[CONSUME]);
+  munmap(structdata->shared_mem, shared_mem_size);
+  close(structdata->fd);
+  shm_unlink(name);     // delete shared memory
+  shm_unlink(name_sem); // delete shared memory
 
   return EXIT_SUCCESS;
 }
+/*	Observations:
+ *  -> If you quit your program clean (close and unlink the file/shared memory segment)
+ *     you can get the correct answer if you are a lucky and n < b. But there is a problem, if N > B you have
+ *     to use the ring buffer. Due to the implementation, the ring buffer doesn't work
+ *     as expected, because it doesn't wait for the reader to read the value and then
+ *     overrides it. It overrides it, nevertheless the reader has read it or not.
+ *
+ *  -> If you try really large numbers for N and B, it is likely to run into an overflow or even the
+ *     program will crash, due to a too big number and end in an SIGSEV.
+ *
+ */
--- e5/task2/task2_old.c
+++ e5/task2/task2_old.c
@@ -1,158 +1,166 @@
-#include <stddef.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
-#include <string.h>
-#include <inttypes.h>
 #include <fcntl.h>
 #include <sys/mman.h>
-#include <sys/stat.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 #include <semaphore.h>
 
-typedef struct data
+void writer(uint64_t n, uint64_t b, uint64_t shared_mem_size, const char *name, sem_t write, sem_t consume)
 {
-  char *name;
-  int fd;
-  uint64_t *shared_mem;
-  sem_t write;
-  sem_t consume;
-} ThreadData;
 
-ThreadData *allocate_ring_buff(const char *name, uint64_t b)
-{
-  char *name = "test1";
+  const int fd = shm_open(name, O_RDWR, S_IRUSR | S_IWUSR);
 
-  const int oflag = O_CREAT | O_EXCL | O_RDWR;
-
-  const mode_t permission = S_IRUSR | S_IWUSR; // 600
-  const int fd = shm_open(name, oflag, permission);
   if (fd < 0)
   {
     perror("shm_open");
-    return NULL;
+    return;
   }
 
-  const size_t shared_mem_size = b * sizeof(uint64_t);
+  uint64_t *shared_mem = mmap(NULL, shared_mem_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
-  if (ftruncate(fd, shared_mem_size) != 0)
+  if (shared_mem == MAP_FAILED)
   {
-    perror("ftruncate");
-    return NULL;
+    perror("mmap");
+    return;
+  }
+
+  for (uint64_t i = 0; i < n; ++i)
+  {
+    sem_wait(&write);
+    if (n >= b)
+      shared_mem[i % b] = i + 1;
+    else
+      shared_mem[i] = i + 1;
+    sem_post(&consume);
+  }
+
+  sem_destroy(&write);
+  sem_destroy(&consume);
+  munmap(shared_mem, shared_mem_size);
+  close(fd);
+}
+
+uint64_t reader(uint64_t n, uint64_t b, uint64_t shared_mem_size, const char *name, sem_t write, sem_t consume)
+{
+  const int fd = shm_open(name, O_RDWR, 0);
+  if (fd < 0)
+  {
+    perror("shm_open");
+    return EXIT_FAILURE;
   }
 
   uint64_t *shared_mem = mmap(NULL, shared_mem_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
   if (shared_mem == MAP_FAILED)
   {
     perror("mmap");
-    return NULL;
+    return EXIT_FAILURE;
   }
-  ThreadData *structdata = malloc(sizeof(structdata));
-  structdata->fd = fd;
-  structdata->shared_mem = shared_mem;
-  structdata->name = name;
 
-  return structdata;
-}
-
-void free_data(uint64_t b, ThreadData *data)
-{
-  munmap(data->shared_mem, b * sizeof(uint64_t));
-  close(data->fd);
-  shm_unlink(data->name);
-  sem_destroy(&data->write);
-  sem_destroy(&data->consume);
-  free(data);
-}
-
-void writer(uint64_t n, uint64_t b, ThreadData *data)
-{
-  for (uint64_t i = 0; i < n; ++i)
-  {
-    sem_wait(&data->write);
-    if (n > b)
-    {
-      data->shared_mem[i % b] = i + 1;
-    }
-    else
-    {
-      data->shared_mem[i] = i + 1;
-    }
-    sem_post(&data->consume);
-  }
-}
-
-uint64_t reader(uint64_t n, uint64_t b, struct data *data)
-{
   uint64_t sum = 0;
   for (uint64_t i = 0; i < n; ++i)
   {
-    sem_wait(&data->consume);
-    if (n > b)
-    {
-      sum += data->shared_mem[i % b];
-    }
+    sem_wait(&consume);
+    if (n >= b)
+      sum += shared_mem[i % b];
     else
-    {
-      sum += data->shared_mem[i];
-    }
-    sem_post(&data->write);
+      sum += shared_mem[i];
+    sem_post(&write);
   }
+
+  sem_destroy(&write);
+  sem_destroy(&consume);
+  munmap(shared_mem, shared_mem_size);
+  close(fd);
   return sum;
 }
 
-int main(int argc, char **argv)
+int initialize_shared_mem(const char *name, const uint64_t shared_mem_size)
 {
+  const int oflag = O_CREAT | O_EXCL | O_RDWR; // fail if name already exists, read+write
+  const mode_t permission = S_IRUSR | S_IWUSR; // 600
+
+  const int fd = shm_open(name, oflag, permission);
+  if (fd < 0)
+  {
+    perror("shm_open");
+    return EXIT_FAILURE;
+  }
+
+  if (ftruncate(fd, shared_mem_size) != 0)
+  {
+    perror("ftruncate");
+    return EXIT_FAILURE;
+  }
+
+  uint64_t *shared_mem = mmap(NULL, shared_mem_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (shared_mem == MAP_FAILED)
+  {
+    perror("mmap");
+    return EXIT_FAILURE;
+  }
+
+  munmap(shared_mem, shared_mem_size);
+  close(fd);
+  return EXIT_SUCCESS;
+}
+
+int main(int argc, char *argv[])
+{
+
   if (argc != 3)
   {
-    printf("usage ./<filename> <N> <B>; replace N and B with int(s)");
     return EXIT_FAILURE;
   }
 
   uint64_t n = strtol(argv[1], NULL, 10);
   uint64_t b = strtol(argv[2], NULL, 10);
 
-  const char *name = "/csaz9802shared_memoryasass";
-  ThreadData *data = allocate_ring_buff(name, b);
+  char *name = "/shared_mem";
+  const uint64_t shared_mem_size = b * sizeof(uint64_t) + 2 * sizeof(sem_t);
 
-  sem_init(&data->write, 0, 1);
-  sem_init(&data->consume, 0, 1);
+  if (initialize_shared_mem(name, shared_mem_size) != EXIT_SUCCESS)
+  {
+    return EXIT_FAILURE;
+  }
+
+  sem_t write;
+  sem_t consume;
+  sem_init(&write, 1, 1);
+  sem_init(&consume, 1, 1);
 
   const pid_t writer_proc = fork();
   if (writer_proc == -1)
-    return EXIT_FAILURE;
-
-  if (writer_proc == 0) // Child 1
   {
-    writer(n, b, data);
-    free_data(b, data);
+    return EXIT_FAILURE;
+  }
+
+  if (writer_proc == 0)
+  {
+    writer(n, b, shared_mem_size, name, write, consume);
     exit(0);
   }
 
   const pid_t reader_proc = fork();
   if (reader_proc == -1)
-    return EXIT_FAILURE;
-
-  if (reader_proc == 0) // Child 2
   {
-    printf("%lu", reader(n, b, data));
-    free_data(b, data);
+    return EXIT_FAILURE;
+  }
+
+  if (reader_proc == 0)
+  {
+    printf("%llu", reader(n, b, shared_mem_size, name, write, consume));
     exit(0);
   }
 
-  wait(0);
-  free_data(b, data);
-}
+  wait(NULL); // wait for both forks
+  wait(NULL);
 
-/*	Observations:
- *  -> If you quit your program clean (close and unlink the file/shared memory segment)
- *     you should get the correct answer. But there is a problem, if N > B you have
- *     to use the ring buffer. Due to the implementation, the ring buffer doesn't work
- *     as expected, because it doesn't wait for the reader to read the value and then
- *     override it. It overrides it, nevertheless the reader has read it or not.
- *
- *  -> If you try really large numbers for N and B, it is likely to run into an overflow or even the
- *     program will crash, due to a too big number and end in an SIGSEV.
- *
- */
+  sem_destroy(&write);
+  sem_destroy(&consume);
+  shm_unlink(name); // delete shared memory
+
+  return EXIT_SUCCESS;
+}
