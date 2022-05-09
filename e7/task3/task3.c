675c3fc6cdd144a22a35cd2b3fa263016a1158ec
Paul Pruenster <paul.pruenster@gmail.com>
2022-05-09 17:47:36 +0200

    commentare

--- e7/task3/task3.c
+++ e7/task3/task3.c
@@ -5,6 +5,7 @@
 
 #define PLAYER_LOST 7
 #define PLAYER_DEAD 8
+
 #define PLAYERS 5
 
 // https://godbolt.org/z/3YdsEvPoe
@@ -54,12 +55,11 @@ void *thread(void *arg)
 
     do
     {
-        if (shared->values[thread_data->thread_id] != PLAYER_DEAD)   
+        if (shared->values[thread_data->thread_id] != PLAYER_DEAD)
             rollTheDice(thread_data);
-        
 
         // wait for each thread to finish rolling the dice and computing the looser(s)
-        if (pthread_barrier_wait(&shared->barrier) == PTHREAD_BARRIER_SERIAL_THREAD) 
+        if (pthread_barrier_wait(&shared->barrier) == PTHREAD_BARRIER_SERIAL_THREAD)
         {
             computeLoser(shared);
 
@@ -101,18 +101,29 @@ int main()
 
     srand(time(NULL));
     if (pthread_barrier_init(&data.barrier, NULL, PLAYERS) != 0)
+    {
+        perror("ERROR while initializing barrier");
         return EXIT_FAILURE;
+    }
 
     for (int i = 0; i < PLAYERS; i++)
     {
         thread_data[i].data = &data;
         thread_data[i].thread_id = i;
         if (pthread_create(&thread_data[i].thread, NULL, thread, (void *)&thread_data[i]) != 0)
+        {
+            perror("ERROR while creating thread");
             return EXIT_FAILURE;
+        }
     }
     for (int i = 0; i < PLAYERS; i++)
         pthread_join(thread_data[i].thread, NULL);
 
     if (pthread_barrier_destroy(&data.barrier) != 0)
+    {
+        perror("ERROR while destroying barrier");
         return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
