edd62b42135bd251ff53024492c5e54ca9836ff2
Paul Pruenster <paul.pruenster@gmail.com>
2022-05-18 21:20:41 +0200

    honns net lossn gekennt honn testen gmiet

--- e9/task2/client.c
+++ e9/task2/client.c
@@ -10,28 +10,33 @@
 pthread_t write_thread;
 pthread_t read_thread;
 
-typedef struct client_data {
-    int sockfd;
-    char * name; 
-} client_data; 
-
-
-void* write_to_server(void * arg)
+typedef struct client_data
 {
-    client_data * cdata = (client_data * ) arg;
+    int sockfd;
+    char *name;
+} client_data;
+
+void *write_to_server(void *arg)
+{
+    client_data *cdata = (client_data *)arg;
     char buff[MAX];
     send(cdata->sockfd, cdata->name, strlen(cdata->name), 0);
     for (;;)
     {
         bzero(buff, sizeof(buff));
         printf("> ");
-        
+
         int n = 0;
         while ((buff[n++] = getchar()) != '\n')
             ;
         if (strncmp("/quit", buff, 5) == 0)
         {
-            perror("schaden");
+            printf("schaden\n");
+            // TODO dor reader thread wert net gekillt nor wortet die main afn sem obwohl es recv eig fahler schmeisen sollet
+            // TODO vlt oenfoch /quit senden und dor server soll in client beenden? sell hot dor oene im ps getun
+
+            // TODO server sendent in folschen client es folsche zuig schunscht passts, client isch /quit hin und es unzoegen
+            fflush(stdout);
             return NULL;
         }
         if (strncmp("/shutdown", buff, 9) == 0)
@@ -44,15 +49,15 @@ void* write_to_server(void * arg)
     }
 }
 
-
-void* read_from_server(void * arg)
+void *read_from_server(void *arg)
 {
     char *buff = malloc(MAX * sizeof(char));
-    client_data * cdata = (client_data * ) arg;
+    client_data *cdata = (client_data *)arg;
     for (;;)
     {
         bzero(buff, strlen(buff));
-        if(!recv(cdata->sockfd, buff, MAX, 0)){
+        if (!recv(cdata->sockfd, buff, MAX, 0))
+        {
             free(buff);
             return NULL;
         }
@@ -60,7 +65,6 @@ void* read_from_server(void * arg)
         fflush(stdout);
     }
 }
-
 
 int main(int argc, char **argv)
 {
@@ -95,13 +99,13 @@ int main(int argc, char **argv)
     }
     else
         printf("connected to the server..\n");
-    
-   // pthread_create(&data->clients[i].thread, NULL, job, (void *)c_data);
-    client_data * cdata = malloc(sizeof(client_data));
+
+    // pthread_create(&data->clients[i].thread, NULL, job, (void *)c_data);
+    client_data *cdata = malloc(sizeof(client_data));
     cdata->name = argv[2];
     cdata->sockfd = sockfd;
-    pthread_create(&write_thread,NULL,write_to_server, (void * )cdata);
-    pthread_create(&read_thread, NULL, read_from_server, (void * ) cdata);
+    pthread_create(&write_thread, NULL, write_to_server, (void *)cdata);
+    pthread_create(&read_thread, NULL, read_from_server, (void *)cdata);
     pthread_join(write_thread, NULL);
     pthread_join(read_thread, NULL);
 
