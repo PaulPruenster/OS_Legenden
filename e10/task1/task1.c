3ff5bc3adfa8f3ceb4f6994433cb457ef7169a32
benno zid <benno.steinegger02@gmail.com>
2022-05-29 18:35:07 +0200

    minor changes

--- e10/task1/task1.c
+++ e10/task1/task1.c
@@ -10,12 +10,11 @@
 
 #define BLOCK_SIZE 1024
 
-pthread_mutex_t mutex;
-void *storage;
+
 struct node
 {
     char memory[BLOCK_SIZE - 16];
-    struct node *next_asdf;
+    struct node *next;
     atomic_bool isFree;
 };
 
@@ -24,6 +23,9 @@ typedef struct my_head_struct
     size_t size; // how many blocks
     struct node *start; // first node
 } head;
+
+pthread_mutex_t mutex;
+head *storage;
 
 void *my_malloc(size_t size)
 {
@@ -43,7 +45,7 @@ void *my_malloc(size_t size)
 
     // set next free node of head
     node->isFree = false;
-    ((head *)storage)->start = node->next_asdf;
+    ((head *)storage)->start = node->next;
 
     // return pointer of the memory
     pthread_mutex_unlock(&mutex);
@@ -55,7 +57,7 @@ void my_free(void *ptr)
 {
     pthread_mutex_lock(&mutex);
 
-    struct node *j;
+    struct node *oldstart;
 
     for (size_t i = 0; ((head *)storage)->size; i++)
     {
@@ -63,39 +65,39 @@ void my_free(void *ptr)
 
         if (node->memory == ptr && !node->isFree)
         {
-            //memset(node->memory, BLOCK_SIZE, 0);
-            j = ((head *)storage)->start;
-            if (j == NULL)
+            //memset(node->memory, (BLOCK_SIZE - 16), 0);
+            oldstart = ((head *)storage)->start;
+            if (oldstart == NULL)
             {
                 ((head *)storage)->start = node;
-                node->next_asdf = NULL;
+                node->next = NULL;
                 pthread_mutex_unlock(&mutex);
                 return;
             }
-            if (j > node)
+            if (oldstart > node)
             {
                 ((head *)storage)->start = node;
-                node->next_asdf = j;
+                node->next = oldstart;
                 pthread_mutex_unlock(&mutex);
                 return;
             }
-            while (j != NULL)
+            while (oldstart != NULL)
             {
-                if (j < node)
+                if (oldstart < node) // |x|o|x|f|n|
                 {
-                    node->next_asdf = j->next_asdf;
-                    j->next_asdf = node;
+                    node->next = oldstart->next;
+                    oldstart->next = node;
                     pthread_mutex_unlock(&mutex);
                     return;
                 }
-                if (j > node)
+                if (oldstart > node)
                 {
-                    j-> next_asdf = node;
-                    node->next_asdf = NULL;
+                    oldstart-> next = node;
+                    node->next = NULL;
                     pthread_mutex_unlock(&mutex);
                     return;
                 }
-                j = j->next_asdf;
+                oldstart = oldstart->next;
             }
         }
     }
@@ -110,22 +112,22 @@ void my_allocator_init(size_t size)
         exit(EXIT_FAILURE);
     }
     pthread_mutex_init(&mutex, NULL);
-    storage  = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    storage  = (head*)mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 
     if (storage == MAP_FAILED)
     {
         perror("mmap failed");
         exit(EXIT_FAILURE);
     }
-    ((head *)storage)->size = size;
-    ((head *)storage)->start =(struct node*) ((ptrdiff_t)storage + sizeof(head));
+    storage->size = size;
+    storage->start = (struct node*) ((ptrdiff_t)storage + sizeof(head));
 
     for (size_t i = 0; i < ((size - sizeof(head)) / BLOCK_SIZE); i++)
     {
-        ((struct node *)((ptrdiff_t)storage + sizeof(head) + (i * sizeof(struct node))))->next_asdf = (struct node*)((ptrdiff_t)storage + sizeof(head) + ((i + 1) * (sizeof(struct node))));
+        ((struct node *)((ptrdiff_t)storage + sizeof(head) + (i * sizeof(struct node))))->next = (struct node*)((ptrdiff_t)storage + sizeof(head) + ((i + 1) * (sizeof(struct node))));
         ((struct node *)((ptrdiff_t)storage + sizeof(head) + (i * sizeof(struct node))))->isFree = true;
     }
-    ((struct node *)((ptrdiff_t)storage + sizeof(head) + ((size / BLOCK_SIZE)-1 * (sizeof(struct node)))))->next_asdf = NULL;
+    ((struct node *)((ptrdiff_t)storage + sizeof(head) + ((size / BLOCK_SIZE)-1 * (sizeof(struct node)))))->next = NULL;
 }
 
 void my_allocator_destroy(void)
