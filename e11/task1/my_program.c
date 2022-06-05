a63d5d03e7301d0caa3d714293ea8dde1cd2025d
Paul Pruenster <paul.pruenster@gmail.com>
2022-06-05 21:01:29 +0200

    joe mama

--- e11/task1/my_program.c
+++ e11/task1/my_program.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int square_my_integer(int x) { return x * x; }
+
 int main(int argc, char *argv[]) {
   if (argc != 2) {
-    printf("usage: ./%s <number>\n", argv[0]);
+    printf("usage: %s <number>\n", argv[0]);
     return EXIT_FAILURE;
   }
-  printf("\n%d\n", atoi(argv[1]));
+  printf("%d\n", square_my_integer(atoi(argv[1])));
   return EXIT_SUCCESS;
 }
